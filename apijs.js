d27167342e88455730037c11d33d3040ac48bccd
VHSR3KorD3R <vhs_rekorder@msn.com>
2023-07-12 21:42:05 +0200

    all carousel and best movie added

--- apijs.js
+++ apijs.js
@@ -30,55 +30,118 @@ const options = {method: 'GET'};
 //   document.getElementsByClassName("test")[0].append(para);
 // }
 
+async function retrieveMovies(url, category) {
+  let response = await fetch(url, options);
+  response = await response.json();
+  console.log(response);
+  const listMovies = response.results;
+
+  for (let movie of listMovies) {
+    let li = document.createElement('li');
+    let img = document.createElement('img');
+    img.src = movie.image_url;
+    li.append(img)
+    li.id = movie.id
+    const ul = document.getElementById(category);
+    ul.appendChild(li);
+  }
+
+  let next = response.next;
+  response = await fetch(next, options);
+  response = await response.json();
+  const listMoviesPage2 = response.results;
+
+  for(let i = 0; i < 2; i++){
+    let li = document.createElement('li');
+    let img = document.createElement('img');
+    img.src = listMoviesPage2[i].image_url;
+    li.append(img)
+    li.id = listMoviesPage2[i].id
+    const ul = document.getElementById(category);
+    ul.appendChild(li);
+  }
+}
 
 let bestMovies = 'http://localhost:8000/api/v1/titles/?sort_by=-imdb_score'
+let bestFrenchMovies = 'http://localhost:8000/api/v1/titles/?country=france&sort_by=-imdb_score'
+let bestActionMovies = 'http://localhost:8000/api/v1/titles/?genre=action&sort_by=-imdb_score'
+let bestScifiMovies = 'http://localhost:8000/api/v1/titles/?genre=sci-fi&sort_by=-imdb_score'
+
+await retrieveMovies(bestMovies, "bestMovies")
+await retrieveMovies(bestFrenchMovies, "bestFrenchMovies")
+await retrieveMovies(bestActionMovies, "bestActionMovies")
+await retrieveMovies(bestScifiMovies, "bestScifiMovies")
 
 let response = await fetch(bestMovies, options);
 response = await response.json();
 const listMovies = response.results;
 
-for (let movie of listMovies) {
-  let li = document.createElement('li');
-  let img = document.createElement('img');
-  img.src = movie.image_url;
-  li.append(img)
-  const ul = document.getElementById("bestMovies");
-  ul.appendChild(li);
+const movieUrl = listMovies[0].url
+response = await fetch(movieUrl, options);
+const movieDesc = response.long_description
+console.log(response);
+
+let img = document.createElement('img');
+img.src = listMovies[0].image_url;
+let div = document.getElementsByClassName("bestMovie")[0];
+let h2 = document.createElement('h2');
+h2.textContent = listMovies[0].title
+let p = document.createElement('p');
+p.textContent = movieDesc
+console.log(div);
+div.id = listMovies[0].id
+div.appendChild(h2)
+div.appendChild(p)
+div.appendChild(img)
+
+// let response = await fetch(bestMovies, options);
+// response = await response.json();
+// const listMovies = response.results;
+
+// for (let movie of listMovies) {
+//   let li = document.createElement('li');
+//   let img = document.createElement('img');
+//   img.src = movie.image_url;
+//   li.append(img)
+//   const ul = document.getElementById("bestMovies");
+//   ul.appendChild(li);
+// }
+
+// let next = response.next;
+// response = await fetch(next, options);
+// response = await response.json();
+// const listMoviesPage2 = response.results;
+
+// for(let i = 0; i < 2; i++){
+//   let li = document.createElement('li');
+//   let img = document.createElement('img');
+//   img.src = listMoviesPage2[i].image_url;
+//   li.append(img)
+//   const ul = document.getElementById("bestMovies");
+//   ul.appendChild(li);
+// }
+
+let carousels = document.querySelectorAll('.carousel');
+for (const carousel of carousels) {
+  let prevButton = carousel.querySelector('.prev');
+  let nextButton = carousel.querySelector('.next');
+  let imageList = carousel.querySelector('ul');
+  let images = carousel.querySelectorAll('li');
+
+  let imageWidth = images[0].offsetWidth;
+  let currentIndex = 0;
+
+  prevButton.addEventListener('click', function () {
+    if (currentIndex > 0) {
+      currentIndex--;
+      imageList.style.left = -currentIndex * imageWidth + 'px';
+    }
+  });
+
+  nextButton.addEventListener('click', function () {
+    if (currentIndex < images.length - 4) {
+      currentIndex++;
+      imageList.style.left = -currentIndex * imageWidth + 'px';
+    }
+  });
 }
-
-let next = response.next;
-response = await fetch(next, options);
-response = await response.json();
-const listMoviesPage2 = response.results;
-
-for(let i = 0; i < 2; i++){
-  let li = document.createElement('li');
-  let img = document.createElement('img');
-  img.src = listMoviesPage2[i].image_url;
-  li.append(img)
-  const ul = document.getElementById("bestMovies");
-  ul.appendChild(li);
-}
-
-var carousel = document.querySelector('.carousel');
-var prevButton = carousel.querySelector('.prev');
-var nextButton = carousel.querySelector('.next');
-var imageList = carousel.querySelector('ul');
-var images = carousel.querySelectorAll('li');
-
-var imageWidth = images[0].offsetWidth;
-var currentIndex = 0;
-
-prevButton.addEventListener('click', function () {
-  if (currentIndex > 0) {
-    currentIndex--;
-    imageList.style.left = -currentIndex * imageWidth + 'px';
-  }
-});
-
-nextButton.addEventListener('click', function () {
-  if (currentIndex < images.length - 4) {
-    currentIndex++;
-    imageList.style.left = -currentIndex * imageWidth + 'px';
-  }
-});
